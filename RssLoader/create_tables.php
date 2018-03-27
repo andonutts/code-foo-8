9a70c75f81a1a5bd5537242498577fe7b12fcc96
hexxx897 <hexxx897@umn.edu>
2018-03-27 03:53:46 -0500

    Add missing table creation queries

--- RssLoader/create_tables.php
+++ RssLoader/create_tables.php
@@ -6,9 +6,8 @@ namespace Andypasti\RssLoader;
  * Creates MySQL tables
  * 
  * Connects to MySQL using the credentials defined in 'mysql_login.php'. If the
- * connection is successful, the tables 'content' and 'content_types' are 
- * dropped if they already exist, then created. Records for the content types 
- * 'article' and 'video' are then inserted into the 'content_type' table.
+ * connection is successful, the tables are dropped if they already exist, then
+ * created.
  * 
  * PHP version 7.2.3
  */
@@ -21,30 +20,90 @@ if (!$conn) {
     die("MySQL connection failed: " . mysqli_connect_error());
 }
 
+mysqli_query($conn, "DROP TABLE IF EXISTS content_tag;");
+mysqli_query($conn, "DROP TABLE IF EXISTS thumbnail_size;");
+mysqli_query($conn, "DROP TABLE IF EXISTS tags;");
+mysqli_query($conn, "DROP TABLE IF EXISTS img_sizes;");
 mysqli_query($conn, "DROP TABLE IF EXISTS content;");
-mysqli_query($conn, "DROP TABLE IF EXISTS content_types;");
+mysqli_query($conn, "DROP TABLE IF EXISTS thumbnails;");
+mysqli_query($conn, "DROP TABLE IF EXISTS file_types;");
+mysqli_query($conn, "DROP TABLE IF EXISTS states;");
+mysqli_query($conn, "DROP TABLE IF EXISTS networks;");
+mysqli_query($conn, "DROP TABLE IF EXISTS categories;");
 
-$querymsg = "CREATE TABLE content_types(
-             content_type_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-             content_type_name VARCHAR(255) UNIQUE NOT NULL,
+$querymsg = "CREATE TABLE categories(
+             category_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+             category_name VARCHAR(255) UNIQUE NOT NULL,
              directory_url VARCHAR(255) UNIQUE NOT NULL
              );";
 
 if (mysqli_query($conn, $querymsg) === true) {
-    echo "Table 'content_types' created successfully\n";
+    echo "Table 'categories' created successfully\n";
 } else {
     echo "Error creating table: " . mysqli_error($conn) . "\n";
 }
 
+$querymsg = "CREATE TABLE networks(
+             network_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+             network_name VARCHAR(255) UNIQUE NOT NULL
+             );";
+
+if (mysqli_query($conn, $querymsg) === true) {
+    echo "Table 'networks' created successfully\n";
+} else {
+    echo "Error creating table: " . mysqli_error($conn) . "\n";
+}
+
+$querymsg = "CREATE TABLE states(
+            state_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+            state_name VARCHAR(255) UNIQUE NOT NULL
+            );";
+
+if (mysqli_query($conn, $querymsg) === true) {
+echo "Table 'states' created successfully\n";
+} else {
+echo "Error creating table: " . mysqli_error($conn) . "\n";
+}
+
+$querymsg = "CREATE TABLE file_types(
+            file_type_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+            file_extension VARCHAR(255) UNIQUE NOT NULL
+            );";
+
+if (mysqli_query($conn, $querymsg) === true) {
+echo "Table 'file_types' created successfully\n";
+} else {
+echo "Error creating table: " . mysqli_error($conn) . "\n";
+}
+
+$querymsg = "CREATE TABLE thumbnails(
+            thumbnail_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+            partial_link VARCHAR(255) UNIQUE NOT NULL,
+            file_type_id INT NOT NULL,
+            FOREIGN KEY (file_type_id) REFERENCES file_types(file_type_id)
+            );";
+
+if (mysqli_query($conn, $querymsg) === true) {
+echo "Table 'thumbnails' created successfully\n";
+} else {
+echo "Error creating table: " . mysqli_error($conn) . "\n";
+}
+
 $querymsg = "CREATE TABLE content(
-             content_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+             content_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
              title VARCHAR(255) NOT NULL,
              description VARCHAR(1024) NOT NULL,
-             content_type_id INT NOT NULL,
              pub_date DATETIME NOT NULL,
-             resource_name VARCHAR(255) NOT NULL,
+             slug VARCHAR(255) NOT NULL,
              guid VARCHAR(255) UNIQUE NOT NULL,
-             FOREIGN KEY (content_type_id) REFERENCES content_types(content_type_id)
+             category_id INT NOT NULL,
+             network_id INT NOT NULL,
+             state_id INT NOT NULL,
+             thumbnail_id INT NOT NULL,
+             FOREIGN KEY (category_id) REFERENCES categories(category_id),
+             FOREIGN KEY (network_id) REFERENCES networks(network_id),
+             FOREIGN KEY (state_id) REFERENCES states(state_id),
+             FOREIGN KEY (thumbnail_id) REFERENCES thumbnails(thumbnail_id)
              );";
 
 if (mysqli_query($conn, $querymsg) === true) {
@@ -53,23 +112,75 @@ if (mysqli_query($conn, $querymsg) === true) {
     echo "Error creating table: " . mysqli_error($conn) . "\n";
 }
 
-$querymsg = "INSERT INTO content_types (content_type_name, directory_url)
+$querymsg = "CREATE TABLE img_sizes(
+    size_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    size_name VARCHAR(255) UNIQUE NOT NULL,
+    width INT NOT NULL,
+    height INT NOT NULL
+    );";
+
+if (mysqli_query($conn, $querymsg) === true) {
+echo "Table 'img_sizes' created successfully\n";
+} else {
+echo "Error creating table: " . mysqli_error($conn) . "\n";
+}
+
+$querymsg = "CREATE TABLE tags(
+    tag_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    tag_name VARCHAR(255) UNIQUE NOT NULL
+    );";
+
+if (mysqli_query($conn, $querymsg) === true) {
+echo "Table 'tags' created successfully\n";
+} else {
+echo "Error creating table: " . mysqli_error($conn) . "\n";
+}
+
+$querymsg = "CREATE TABLE thumbnail_size(
+    thumbnail_id INT NOT NULL,
+    size_id INT NOT NULL,
+    PRIMARY KEY (thumbnail_id, size_id),
+    FOREIGN KEY (thumbnail_id) REFERENCES thumbnails(thumbnail_id),
+    FOREIGN KEY (size_id) REFERENCES img_sizes(size_id)
+    );";
+
+if (mysqli_query($conn, $querymsg) === true) {
+echo "Table 'thumbnail_size' created successfully\n";
+} else {
+echo "Error creating table: " . mysqli_error($conn) . "\n";
+}
+
+$querymsg = "CREATE TABLE content_tag(
+    content_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    PRIMARY KEY (content_id, tag_id),
+    FOREIGN KEY (content_id) REFERENCES content(content_id),
+    FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
+    );";
+
+if (mysqli_query($conn, $querymsg) === true) {
+echo "Table 'content_tag' created successfully\n";
+} else {
+echo "Error creating table: " . mysqli_error($conn) . "\n";
+}
+/*
+$querymsg = "INSERT INTO categories (content_type_name, directory_url)
              VALUES ('article', 'http://www.ign.com/articles/');";
 
 if (mysqli_query($conn, $querymsg) === true) {
-    echo "Record 'article' added to 'content_types' successfully\n";
+    echo "Record 'article' added to 'categories' successfully\n";
 } else {
     echo "Error: " . $querymsg . "\n" . mysqli_error($conn);
 }
 
-$querymsg = "INSERT INTO content_types (content_type_name, directory_url)
+$querymsg = "INSERT INTO categories (content_type_name, directory_url)
              VALUES ('video', 'http://www.ign.com/videos/');";
 
 if (mysqli_query($conn, $querymsg) === true) {
-    echo "Record 'video' added to 'content_types' successfully\n";
+    echo "Record 'video' added to 'categories' successfully\n";
 } else {
     echo "Error: " . $querymsg . "\n" . mysqli_error($conn);
 }
-
+*/
 mysqli_close($conn);
 ?>
