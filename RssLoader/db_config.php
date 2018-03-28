4d507e6daf9e712dcc00578c6503470a9fa704c1
Andy He <smerchy@gmail.com>
2018-03-28 00:12:35 -0500

    Add files via upload

---
+++ RssLoader/db_config.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Andypasti\RssLoader;
+
+/**
+ * Database login credentials
+ * 
+ * Contains login information used to access and update the database.
+ * 
+ * PHP version 7.2.3
+ */
+
+$servername = 'servername';
+$username = 'username';
+$password = 'password';
+$dbname = 'dbname';
+?>
